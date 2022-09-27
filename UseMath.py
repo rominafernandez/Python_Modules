9052d5fb20c79b7ff60d882fbbeaca90add63517
Romina Fernandez <rominafernandez@outlook.de>
2022-09-27 15:11:01 +0200

    Added code for the plot part of the exercise and extended the function to save all results in a list.

--- UseMath.py
+++ UseMath.py
@@ -6,48 +6,128 @@
 # Print out the results
 # Combine all results in a list and create either a Scatter or a Line plot with them (optional)
 import math
+import matplotlib.pyplot as plt
+
+def Math_Ops(x,y):
+    result = list()
+    value = math.exp(x)
+    result.append(value)
+    print(f"e to the power of {x} is {math.exp(x)}")
+
+    value = math.exp(y)
+    result.append(value)
+    print(f"e to the power of {y} is {math.exp(y)}")
+
+    value = math.log(x)
+    result.append(value)
+    print(f"log of {x} is {math.log(x)}")
+
+    value = math.log(y)
+    result.append(value)
+    print(f"log of {y} is {math.log(y,x)}")
+
+    value = math.cos(x)
+    result.append(value)
+    print(f"The cosine of {x} is {math.cos(x)}")
+
+    value = math.cos(y)
+    result.append(value)
+    print(f"The cosine of {y} is {math.cos(y)}")
+
+    value = math.sin(x)
+    result.append(value)
+    print(f"The sinus of {x} is {math.sin(x)}")
+
+    value = math.sin(y)
+    result.append(value)
+    print(f"The sinus of {y} is {math.sin(y)}")
+
+    value = math.tan(x)
+    result.append(value)
+    print(f"The tangens of {x} is {math.tan(x)}")
+
+    value = math.tan(y)
+    result.append(value)
+    print(f"The tangens of {y} is {math.tan(y)}")
+
+    if x > 0:
+        value = math.pow(x,y)
+        result.append(value)
+        print(f"{x} to the power of {y} is {math.pow(x,y)}")
+
+        value = math.log2(x)
+        result.append(value)
+        print(f"log of {x} to the base of 2 is {math.log2(x)}")
+
+        value = math.log10(x)
+        result.append(value)
+        print(f"log of {x} to the base of 10 is {math.log10(x)}")
+
+    if y > 0:
+        value = math.pow(y,x)
+        result.append(value)
+        print(f"{y} to the power of {x} is {math.pow(y,x)}")
+
+        value = math.log2(y)
+        result.append(value)
+        print(f"log of {y} to the base of 2 is {math.log2(y)}")
+
+        value = math.log10(y)
+        result.append(value)
+        print(f"log of {y} to the base of 10 is {math.log10(y)}")
+
+    if x >= 0:
+        value = math.sqrt(x)
+        result.append(value)
+        print(f"The square root of {x} is {math.sqrt(x)}")
+        
+    if y >= 0:
+        value = math.sqrt(y)
+        result.append(value)
+        print(f"The square root of {y} is {math.sqrt(y)}")
+
+    if x > -1 and x < 1:
+        value = math.acos(x)
+        result.append(value)
+        print(f"The acosine of {x} is {math.acos(x)}")
+
+        value = math.asin(x)
+        result.append(value)
+        print(f"The asinus of {x} is {math.asin(x)}")
+
+    if y > -1 and y < 1:
+        value = math.acos(y)
+        result.append(value)
+        print(f"The acosine of {y} is {math.acos(y)}")
+
+        value = math.asin(y)
+        result.append(value)
+        print(f"The asinus of {y} is {math.asin(y)}")
+
+    if x != 0:
+        value = math.atan(x)
+        result.append(value)
+        print(f"The atangens of {x} is {math.atan(x)}")
+
+    if y != 0:
+        value = math.atan(y)
+        result.append(value)
+        print(f"The atangens of {y} is {math.atan(y)}")
+    return result
+
 
 x = int(input("X: "))
 y = int(input("Y: "))
+result = Math_Ops(x,y)
 
-if x > 1 and y > 1:
-    print(f"e to the power of {x} is {math.exp(x)}")
-    print(f"e to the power of {y} is {math.exp(y)}")
-    print(f"{x} to the power of {y} is {math.pow(x,y)}")
-    print(f"{y} to the power of {x} is {math.pow(y,x)}")
-
-    print(f"log of {x} to the base of {y} is {math.log(x,y)}")
-    print(f"log of {y} to the base of {x} is {math.log(y,x)}")
-    print(f"log of {x} to the base of 2 is {math.log2(x)}")
-    print(f"log of {y} to the base of 2 is {math.log2(y)}")
-    print(f"log of {x} to the base of 10 is {math.log2(x)}")
-    print(f"log of {y} to the base of 10 is {math.log2(y)}")
-
-    print(f"The square root of {x} is {math.sqrt(x)}")
-    print(f"The square root of {y} is {math.sqrt(y)}")
-
-    print(f"The cosine of {x} is {math.cos(x)}")
-    print(f"The cosine of {y} is {math.cos(y)}")
-    print(f"The sinus of {x} is {math.sin(x)}")
-    print(f"The sinus of {y} is {math.sin(y)}")
-    print(f"The tangens of {x} is {math.tan(x)}")
-    print(f"The tangens of {y} is {math.tan(y)}")
-
-if x <= 1:
-    print(f"The acosine of {x} is {math.acos(x)}")
-    print(f"The asinus of {x} is {math.asin(x)}")
-    print(f"The atangens of {x} is {math.atan(x)}")
-
-if y <= 1:
-    print(f"The acosine of {y} is {math.acos(y)}")
-    print(f"The asinus of {y} is {math.asin(y)}")
-    print(f"The atangens of {y} is {math.atan(y)}")
+plt.scatter(result, range(len(result)))
+plt.show()
 
 ## Exercise 36 - Working with constants
 
 # Write a second program that takes again two numbers from the user and calculates
 # new values with the help of the same functions as in exercise 35
-#This time the input variables have to multiplied by one of three shown constants (Pi, Euler number or Tau)
+# This time the input variables have to multiplied by one of three shown constants (Pi, Euler number or Tau)
 # Let the user choose which constant should be used
 # Print out the results
 # Combine all results in a list and create either a Scatter or a Line plot with them (optional)
@@ -73,35 +153,7 @@ while invalid_input == True:
 
 print(f"Your new values are {x} and {y}")
 
-if x > 1 and y > 1:
-    print(f"e to the power of {x} is {math.exp(x)}")
-    print(f"e to the power of {y} is {math.exp(y)}")
-    print(f"{x} to the power of {y} is {math.pow(x,y)}")
-    print(f"{y} to the power of {x} is {math.pow(y,x)}")
+result = Math_Ops(x,y)
 
-    print(f"log of {x} to the base of {y} is {math.log(x,y)}")
-    print(f"log of {y} to the base of {x} is {math.log(y,x)}")
-    print(f"log of {x} to the base of 2 is {math.log2(x)}")
-    print(f"log of {y} to the base of 2 is {math.log2(y)}")
-    print(f"log of {x} to the base of 10 is {math.log2(x)}")
-    print(f"log of {y} to the base of 10 is {math.log2(y)}")
-
-    print(f"The square root of {x} is {math.sqrt(x)}")
-    print(f"The square root of {y} is {math.sqrt(y)}")
-
-    print(f"The cosine of {x} is {math.cos(x)}")
-    print(f"The cosine of {y} is {math.cos(y)}")
-    print(f"The sinus of {x} is {math.sin(x)}")
-    print(f"The sinus of {y} is {math.sin(y)}")
-    print(f"The tangens of {x} is {math.tan(x)}")
-    print(f"The tangens of {y} is {math.tan(y)}")
-
-if x <= 1:
-    print(f"The acosine of {x} is {math.acos(x)}")
-    print(f"The asinus of {x} is {math.asin(x)}")
-    print(f"The atangens of {x} is {math.atan(x)}")
-
-if y <= 1:
-    print(f"The acosine of {y} is {math.acos(y)}")
-    print(f"The asinus of {y} is {math.asin(y)}")
-    print(f"The atangens of {y} is {math.atan(y)}")
+plt.scatter(result, range(len(result)))
+plt.show()
