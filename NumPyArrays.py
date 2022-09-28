c824765570d5419a8a9d86be83cba78986232753
Romina Fernandez <rominafernandez@outlook.de>
2022-09-28 09:14:28 +0200

    Added exercise 68 to the program (Generating bar- and line-plots)

--- NumPyArrays.py
+++ NumPyArrays.py
@@ -9,10 +9,12 @@
 # A transposed version of the two-dimensional array TD
 # Write furthermore an algorithm to print out the sum, maximum value, minimum value and shape of all created arrays
 
+
 ## Exercise 80 - Mathematics with NumPy
 
 # Use the arrays from Exercise 38 and run some mathematical functions like np.sin with them
 import numpy as np
+import matplotlib.pyplot as plt
 
 Ar1 = np.zeros(25)
 Ar2 = np.ones(8)
@@ -23,9 +25,12 @@ Ar6 = Ar5.transpose()
 
 def Array_Calc(input_array):
     print(f"Input array: \n{input_array}")
-    print(f"Sum: {input_array.sum()}")
-    print(f"Max: {input_array.max()}")
-    print(f"Min: {input_array.min()}")
+    sum_array = input_array.sum()
+    print(f"Sum: {sum_array}")
+    max_array = input_array.max()
+    print(f"Max: {max_array}")
+    min_array = input_array.min()
+    print(f"Min: {min_array}")
     print(f"Shape: {input_array.shape}")
     print(f"Sinus: \n{np.sin(input_array)}")
     print(f"Cosinus: \n{np.cos(input_array)}")
@@ -37,10 +42,33 @@ def Array_Calc(input_array):
     print(f"To the power of 2: \n{np.power(input_array, 2)}")
     print(f"Fourier Transformation: \n{np.fft.fft(input_array)}")
     print()
+    return sum_array, min_array, max_array
 
 Array_Calc(Ar1)
 Array_Calc(Ar2)
 Array_Calc(Ar3)
-Array_Calc(Ar4)
+sum_array, min_array, max_array = Array_Calc(Ar4)
 Array_Calc(Ar5)
 Array_Calc(Ar6)
+
+## Exercise 68 - Generating Plots
+
+# Use the calculated sums, minimal and maximal values from Exercise 38 to create some
+# bar plots with the help of Matplotlib
+# Furthermore create two line plots for the third and fourth array of Exercise 38
+# Numbers 0 to 24
+# 9 different numbers of your choice
+
+# Bar-Plot for Ar4
+x = ["Sum", "Min", "Max"]
+y = [sum_array, min_array, max_array]
+
+plt.bar(x,y)
+plt.show()
+
+# Line-Plots for Ar3 and Ar4
+plt.plot(Ar3)
+plt.show()
+
+plt.plot(Ar4)
+plt.show()
